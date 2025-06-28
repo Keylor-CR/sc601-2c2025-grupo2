7eff49877d67bfb56a4233b78da7604840baf3be
Keylor Chacon <keylorchs@gmail.com>
2025-06-27 20:57:35 -0600

    Added base Entities

---
+++ SinpeEmpresarial/SinpeEmpresarial.Domain/Entities/Caja.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinpeEmpresarial.Domain.Entities
+{
+    public class Caja
+    {
+        public int IdCaja { get; set; }
+        public int IdComercio { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string TelefonoSINPE { get; set; }
+        public DateTime FechaDeRegistro { get; set; }
+        public DateTime? FechaDeModificacion { get; set; }
+        public bool Estado { get; set; }
+    }
+}
---
+++ SinpeEmpresarial/SinpeEmpresarial.Domain/Entities/Sinpe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinpeEmpresarial.Domain.Entities
+{
+    public class Sinpe
+    {
+        public int IdSinpe { get; set; }
+        public string TelefonoOrigen { get; set; }
+        public string NombreOrigen { get; set; }
+        public string TelefonoDestinatario { get; set; }
+        public string NombreDestinatario { get; set; }
+        public float Monto { get; set; }
+        public DateTime FechaDeRegistro { get; set; }
+        public string Descripcion { get; set; }
+        public bool Estado { get; set; }
+    }
+}
