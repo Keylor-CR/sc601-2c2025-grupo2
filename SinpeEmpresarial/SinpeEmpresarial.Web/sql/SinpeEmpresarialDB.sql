a3154d01a85ab25d89ef8bdcbed0d89dc634582a
marvcs <99229786+marvcs@users.noreply.github.com>
2025-06-29 11:01:42 -0600

    inserts

--- SinpeEmpresarial/SinpeEmpresarial.Web/sql/SinpeEmpresarialDB.sql
+++ SinpeEmpresarial/SinpeEmpresarial.Web/sql/SinpeEmpresarialDB.sql
@@ -61,4 +61,30 @@ VALUES
 ('3-333-555555', 2, 'Soda Mary', 2, '2288-7766', 'admin@sodamary.cr', 'Heredi', GETDATE(), 1),
 ('1-1111-2222', 1, 'Ferreteria Brenes', 3, '2299-8899', 'ventas@ferrebrennes.com', 'Heredia', GETDATE(), 1);
 
-select * from Comercios
+
+INSERT INTO Cajas (IdComercio, Nombre, Descripcion, TelefonoSINPE, FechaDeRegistro, Estado)
+VALUES 
+(1, 'Caja Principal', 'Caja central para atención general', '88881234', GETDATE(), 1),
+(2, 'Caja 1', 'Caja secundaria en Soda Mary', '88884567', GETDATE(), 1),
+(3, 'Caja de herramientas', 'Caja exclusiva para ventas de ferretería', '88889876', GETDATE(), 1);
+
+-- Sinpe to Caja 1 (TelefonoDestinatario = '88881234')
+INSERT INTO Sinpes (TelefonoOrigen, NombreOrigen, TelefonoDestino, NombreDestino, Monto, FechaDeRegistro, Descripcion, Estado)
+VALUES 
+('70001111', 'Cliente A', '88881234', 'Caja Principal', 1500.00, GETDATE(), 'Pago desayuno', 1),
+('70002222', 'Cliente B', '88881234', 'Caja Principal', 2750.00, GETDATE(), 'Pago almuerzo', 1),
+('70003333', 'Cliente C', '88881234', 'Caja Principal', 3990.00, GETDATE(), 'Pago cena', 1);
+
+-- Sinpe to Caja 2 (TelefonoDestinatario = '88884567')
+INSERT INTO Sinpes (TelefonoOrigen, NombreOrigen, TelefonoDestino, NombreDestino, Monto, FechaDeRegistro, Descripcion, Estado)
+VALUES 
+('70004444', 'Cliente D', '88884567', 'Caja 1', 1850.00, GETDATE(), 'Refresco', 1),
+('70005555', 'Cliente E', '88884567', 'Caja 1', 3050.00, GETDATE(), 'Combo especial', 1),
+('70006666', 'Cliente F', '88884567', 'Caja 1', 2200.00, GETDATE(), 'Postre', 1);
+
+-- Sinpe to Caja 3 (TelefonoDestinatario = '88889876')
+INSERT INTO Sinpes (TelefonoOrigen, NombreOrigen, TelefonoDestino, NombreDestino, Monto, FechaDeRegistro, Descripcion, Estado)
+VALUES 
+('70007777', 'Cliente G', '88889876', 'Caja de herramientas', 10400.00, GETDATE(), 'Compra taladro', 1),
+('70008888', 'Cliente H', '88889876', 'Caja de herramientas', 8450.00, GETDATE(), 'Compra tornillos', 1),
+('70009999', 'Cliente I', '88889876', 'Caja de herramientas', 13200.00, GETDATE(), 'Compra combo herramientas', 1);
