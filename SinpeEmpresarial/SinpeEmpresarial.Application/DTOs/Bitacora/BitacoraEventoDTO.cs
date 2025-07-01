f12a02716b97c0a10737cecd9ee3b731ecedde72
kennier-t <kennier.trejos@gmail.com>
2025-07-01 02:07:10 -0600

    mejora de ui

--- SinpeEmpresarial/SinpeEmpresarial.Application/DTOs/Bitacora/BitacoraEventoDTO.cs
+++ SinpeEmpresarial/SinpeEmpresarial.Application/DTOs/Bitacora/BitacoraEventoDTO.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +9,21 @@ namespace SinpeEmpresarial.Application.DTOs.Bitacora
 {
     public class BitacoraEventoDTO
     {
+        [Display(Name="ID")]
         public int IdEvento { get; set; }
+        [Display(Name="Tabla")]
         public string TablaDeEvento { get; set; }
+        [Display(Name="Tipo")]
         public string TipoDeEvento { get; set; }
+        [Display(Name="Fecha")]
         public DateTime FechaDeEvento { get; set; }
+        [Display(Name="Descripcion")]
         public string DescripcionDeEvento { get; set; }
+        [Display(Name="Traza")]
         public string StackTrace { get; set; }
+        [Display(Name="Datos Anteriores")]
         public string DatosAnteriores { get; set; }
+        [Display(Name="Datos Posteriores")]
         public string DatosPosteriores { get; set; }
     }
 }
